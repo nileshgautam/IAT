a661c77bf21c4f0e8c29dc7459794b984bff3325
Nilesh <nilesh@gmail.com>
2020-03-07 17:51:33 +0530

    name helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','process_menu','datefilter');
+$autoload['helper'] = array('url','form','process_menu','dateFilter');
 
 /*
 | -------------------------------------------------------------------
