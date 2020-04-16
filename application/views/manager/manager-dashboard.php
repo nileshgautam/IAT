cc0db5e3c747b432d31b8f15a8034507d5f5e227
Nilesh <ngautam18@gmail.com>
2020-04-16 14:09:52 +0530

    manager dashboard chart complete work steps and pending work steps are done first phase

--- application/views/manager/manager-dashboard.php
+++ application/views/manager/manager-dashboard.php
@@ -7,8 +7,29 @@
                 </div>
             </div>
         </div>
-        <div id="chart_div" ></div>
+        <div id="chart_div"></div>
 
-        <div id="selected-row"></div>
+        <div class="card mb-5 complete" style="display:none">
+            <div class="card-header p-2">
+                <h3 class="">Complete</h3>
+            </div>
+            <div class="card-body" id="client-card-body">
+                <div id="totla-complete-order" class="clients">
+                </div>
+                <!-- <div id="donut"></div> -->
+            </div>
+        </div>       
+        <div class="card mb-5 under-process" style="display:none">
+            <div class="card-header p-2">
+                <h3 class="">Under process</h3>
+            </div>
+            <div class="card-body" id="client-card-body">
+                <div id="totla-underprocess-wo" class="clients">
+                </div>
+            </div>
+        </div>
+
+
+
     </div>
 </div>
